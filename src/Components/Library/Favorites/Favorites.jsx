d38184c1822a287f08dc3de7bd248c43416c52b0
wolfrain696 <66623494+wolfrain696@users.noreply.github.com>
2021-08-03 15:23:13 +0300

    добавил данные авторов

--- src/Components/Library/Favorites/Favorites.jsx
+++ src/Components/Library/Favorites/Favorites.jsx
@@ -1,8 +1,9 @@
 import S from './Favorites.module.css'
 import heartImg from '../../../img/heart.svg'
+import {AuthorsJSON} from '../../../Fetch/Authors'
 
 
-export const Favorites = (props) => {
+export const Favorites = ({onData}) => {
     return (
         <div className={S.container}>
             <div className={S.favorites}>
@@ -11,8 +12,8 @@ export const Favorites = (props) => {
             </div>
             <div className={S.category}>
                 <ul className={S.category__list}>
-                    <li className={S.category__item}>
-                        Романы
+                    <li className={S.category__item} onClick={() => onData(AuthorsJSON)}>
+                        Авторы
                     </li>
                     <li className={S.category__item}>
                         Детективы
