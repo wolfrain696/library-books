91e418798818226b768535c4304bdb4fa2a2c2d2
Кристина <1059700@bsu.edu.ru>
2021-08-07 17:55:44 +0300

    Изменение поиска, для работы с избранным

--- src/Components/Library/Search/Search.jsx
+++ src/Components/Library/Search/Search.jsx
@@ -11,15 +11,17 @@ export const Search = ({data, changePage, page, favorites}) => {
 
   const [val, setVal] = useState('')
 
-  let filteredElements
+  let filteredElements = []
   let ShowList
 
   if (isCollectionNotEmpty(data)) {
-    if (isBook(data.docs[0])) {
-      filteredElements = filterByTitle(data.docs)
-    } else if (isAuthor(data.docs[0])) {
-      filteredElements = filterByAuthorName(data.docs)
-    }
+    data.docs.forEach(element => {
+      if (isBook(element) && filterByTitle(element)) {
+        filteredElements.push(element)
+      } else if (isAuthor(element) && filterByAuthorName(element)) {
+        filteredElements.push(element)
+      }
+    })
     ShowList = filteredElements.map((element) =>
       <BookCard page={page} favorites={favorites} changePage={changePage} item={element} authorPhoto={element.key}
                 key={element.key}
@@ -27,59 +29,51 @@ export const Search = ({data, changePage, page, favorites}) => {
     )
   }
 
-function isCollectionNotEmpty(collection) {
-return collection.docs.length !== 0
-}
+  function isCollectionNotEmpty(collection) {
+    return collection.docs.length !== 0
+  }
 
-function isBook(element) {
-return element.type === 'work'
-}
+  function isBook(element) {
+    return element.type === 'work'
+  }
 
-function isAuthor(element) {
-return element.type === 'author'
-}
+  function isAuthor(element) {
+    return element.type === 'author'
+  }
 
-function filterByTitle(collection) {
-return collection.filter(element =>
-  {
+  function filterByTitle(element) {
     return element.title.toLowerCase().includes(val.toLowerCase())
   }
-,
-)
-}
 
-function filterByAuthorName(collection)
-  {
-    return collection.filter(element => {
-      return element.name.toLowerCase().includes(val.toLowerCase())
-    })
+  function filterByAuthorName(element) {
+    return element.name.toLowerCase().includes(val.toLowerCase())
   }
 
 
-return (
-  <div className={S.content}>
-    <form>
-      <div className={S.search}>
-        <img src={searchImg} alt='search' />
-        <input
-          placeholder='Поиск...'
-          onChange={(event) => setVal(event.target.value)} />
-        <Button
-          onClick={() => {
-            alert('Отфильтруем книги?')
-          }}
-        >
-          <img src={filterImg} alt='filter' />
-        </Button>
-      </div>
-    </form>
-    <ul className={S.searchList}>
-      {
-        ShowList
-      }
-    </ul>
-  </div>
-)
+  return (
+    <div className={S.content}>
+      <form>
+        <div className={S.search}>
+          <img src={searchImg} alt='search' />
+          <input
+            placeholder='Поиск...'
+            onChange={(event) => setVal(event.target.value)} />
+          <Button
+            onClick={() => {
+              alert('Отфильтруем книги?')
+            }}
+          >
+            <img src={filterImg} alt='filter' />
+          </Button>
+        </div>
+      </form>
+      <ul className={S.searchList}>
+        {
+          ShowList
+        }
+      </ul>
+    </div>
+  )
 }
 
 
